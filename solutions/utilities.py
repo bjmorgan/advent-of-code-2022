c00d04939aacb24d7e3c16257f94d90d2b3db364
Benjamin <b.j.morgan@bath.ac.uk>
2022-12-23 18:28:34 +0000

    Day 22 part one

---
+++ solutions/utilities.py
@@ -0,0 +1,9 @@
+def xy_array_as_string(array,
+                       mapping: dict[int, str],
+                       hspace: Optional[bool]=False) -> str:
+    string_array = np.empty_like(array, dtype=str)
+    for i, row in enumerate(array):
+        for j, n in enumerate(row):
+            string_array[i, j] = mapping.get(n)
+    hjoin = ' ' if hspace else ''
+    return '\n'.join([hjoin.join(row) for row in string_array.T])
